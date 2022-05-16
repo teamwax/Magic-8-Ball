1acae5c11c353cb81c6b99b1146085a749bd9713
teamwax <105081845+teamwax@users.noreply.github.com>
2022-05-16 06:19:38 -0700

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,47 @@
+@import 
+url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+.shake{
+    animation: shake 1s;
+}
+
+  @keyframes shake {
+    0% { transform: translate(4px, 4px) rotate(0deg); }
+    10% { transform: translate(-4px, -4px) rotate(-1deg); }
+    20% { transform: translate(-7px, 0px) rotate(1deg); }
+    30% { transform: translate(7px, 4px) rotate(0deg); }
+    40% { transform: translate(4px, -4px) rotate(1deg); }
+    50% { transform: translate(-4px, 4px) rotate(-1deg); }
+    60% { transform: translate(-7px, 4px) rotate(0deg); }
+    70% { transform: translate(7px, 4px) rotate(-1deg); }
+    80% { transform: translate(-4px, -4px) rotate(1deg); }
+    90% { transform: translate(4px, 4px) rotate(0deg); }
+    100% { transform: translate(4px, -4px) rotate(-1deg); }
+  }
+
+  body{
+      background-color: #786ba2;
+      color:rgb(186, 214, 30);
+      font-family: 'Roboto Mono', monospace;
+  }
+  img{
+      padding-left:2em;
+  }
+  button{
+      margin-top: 1em;
+  }
+
+  .center{
+      text-align: center;
+      padding-top:2em;
+      
+  }
+
+  h1{
+      font-size: 4em;
+  }
+
+  #message{
+      font-size: 2em;
+      margin-top: 4em;
+      margin-left:2em;
+  }
